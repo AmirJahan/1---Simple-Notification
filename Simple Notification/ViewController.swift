2480052bc9b15bfe34dbbc1b865dbde900b6a6fc
Amir J <amir.jahan@gmail.com>
2017-11-06 11:10:34 -0800

    first commit

--- Simple Notification/ViewController.swift
+++ Simple Notification/ViewController.swift
@@ -1,25 +1,160 @@
-//
-//  ViewController.swift
-//  Simple Notification
-//
-//  Created by Amir on 11/5/17.
-//  Copyright © 2017 Cinard Digital Media Institute. All rights reserved.
-//
 
 import UIKit
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UNUserNotificationCenterDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        // get the permission first
+        getUserPermission();
+        
+        // Simple Notification
+        configureSimpleNotification()
+        showSimpleNotification()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    
+    
+    
+    
+    
+    private func configureSimpleNotification() {
+        UNUserNotificationCenter.current().delegate = self
+        
+        let action_1 = UNNotificationAction(identifier: "actId_1",
+                                            title: "Read Later",
+                                            options: [])
+        let action_2 = UNNotificationAction(identifier: "actId_2",
+                                            title: "Show Details",
+                                            options: [.foreground])
+        
+        let myCategory = UNNotificationCategory(identifier: "catId",
+                                                actions: [action_1, action_2],
+                                                intentIdentifiers: [],
+                                                options: [])
+        
+        UNUserNotificationCenter.current().setNotificationCategories([myCategory])
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    func showSimpleNotification() {
+        let simpleTrigger = UNTimeIntervalNotificationTrigger(timeInterval: 8,
+                                                              repeats: false)
+        let simpleContent = UNMutableNotificationContent()
+        simpleContent.title = "Simple Notification"
+        simpleContent.body = "This should happen in 8\"!"
+        simpleContent.sound = UNNotificationSound.default()
+        
+        simpleContent.categoryIdentifier = "catId"
+        
+        
+        
+        let path = Bundle.main.path(forResource: "cinard",
+                                    ofType: "png")
+        
+        let url = URL(fileURLWithPath: path!)
+        
+        do {
+            let attachment = try UNNotificationAttachment(identifier: "simpleIdentifier",
+                                                          url: url,
+                                                          options: nil)
+            simpleContent.attachments = [attachment]
+        } catch {
+            print("Error with attachement")
+        }
 
-
+        
+        let request = UNNotificationRequest(identifier:"simpleId",
+                                            content: simpleContent,
+                                            trigger: simpleTrigger)
+        
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        UNUserNotificationCenter.current().add(request) {(error) in
+            if let error = error {
+                print("error with request: \(error)")
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if ( segue.identifier == "goSegue")
+        {
+            // we reached there through notification
+            
+        }
+    }
+    
+    
+    
+    // delegate methods
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        switch response.actionIdentifier {
+        case "actId_1":
+            print("Action one")
+        //
+        case "actId_2":
+            print("Action two")
+            self.performSegue(withIdentifier: "goSegue",
+                              sender: nil)
+        default:
+            print("Other Action")
+        }
+        completionHandler()
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    func getUserPermission()
+    {
+        // grant access by user to receve notifications
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(options: [.alert, .sound])
+            {
+                (accepted, error) in
+                var alert : UIAlertController;
+                
+                
+                if !accepted
+                {
+                    alert = UIAlertController(title: "Denied",
+                                              message: "Notification access denied.",
+                                              preferredStyle: UIAlertControllerStyle.alert);
+                }
+                else
+                {
+                    alert = UIAlertController(title: "Granted",
+                                              message: "Notification access granted.",
+                                              preferredStyle: UIAlertControllerStyle.alert);
+                }
+                alert.addAction(UIAlertAction(title: "Ok",
+                                              style: UIAlertActionStyle.default,
+                                              handler: nil))
+                
+                self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
